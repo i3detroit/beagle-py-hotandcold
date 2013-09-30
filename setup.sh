408f56e34255c251065935c942765dd5af1686ba
Jason Kridner <jdk@ti.com>
2013-09-30 16:09:14 +0000

    Add some setup files

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+mount /dev/mmcblk0p1 /mnt cp uEnv.txt /mnt/ cp getty\@tty1.service 
+/etc/systemd/system/getty-target-wants/ systemctl --system daemon-reload 
+systemctl disable gdm.service systemctl stop gdm.service systemctl 
+restart getty\@tty1.service opkg update opkg install python-misc 
+python-compiler
