c1004266d1c28b1287a69a7132ae9d1cb550c28f
Jason Kridner <jdk@ti.com>
2013-09-30 16:12:00 +0000

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-mount /dev/mmcblk0p1 /mnt cp uEnv.txt /mnt/ cp getty\@tty1.service 
-/etc/systemd/system/getty-target-wants/ systemctl --system daemon-reload 
-systemctl disable gdm.service systemctl stop gdm.service systemctl 
-restart getty\@tty1.service opkg update opkg install python-misc 
-python-compiler
+mount /dev/mmcblk0p1 /mnt
+cp uEnv.txt /mnt/
+cp getty\@tty1.service /etc/systemd/system/getty-target-wants/
+systemctl --system daemon-reload 
+systemctl disable gdm.service
+systemctl stop gdm.service
+systemctl restart getty\@tty1.service
+opkg update
+opkg install python-misc python-compiler
+systemctl enable dropbear.socket
+systemctl start dropbear.socket
